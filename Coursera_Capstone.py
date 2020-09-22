d71d671582c1ae1264c08d3e9d28d1a3d4504a45
Lash-360 <aghafejiro@yahoo.com>
2020-09-22 01:40:27 +0100

    Coursera_Capstone.py

---
+++ Coursera_Capstone.py
@@ -0,0 +1,6 @@
+##This Notebook has been built for the Coursera Capstone Project. It will using data vehiclar collision as case study.
+
+import pandas as pd
+import numpy as np
+
+print('Hello Capstone Project Course!')
